62c5ba2e6056c0d1873164593cd1a507f7001381
unknown <adiishukla196@gmail.com>
2021-01-02 04:11:37 +0530

    doing well

---
+++ noteApp/note.js
@@ -0,0 +1,15 @@
+const notes = [
+    {
+    title:'my next trip',
+    body:'i would like to go spain'
+    },
+    {
+        title:'Habbits to work on',
+        body:'exercise, eating a bit better'
+        },
+        {
+            title:'Office modification',
+            body:'get a new seat'
+            },
+
+]
