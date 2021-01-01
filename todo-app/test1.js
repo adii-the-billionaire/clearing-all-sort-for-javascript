a7cee74849fae480aa0aa80a1bfc3f98938f35f0
unknown <adiishukla196@gmail.com>
2021-01-02 02:27:44 +0530

    doomsday

--- todo-app/test1.js
+++ todo-app/test1.js
@@ -15,15 +15,7 @@ const work = [
         text:'Do work',
         completed:true
     }]
-    //here is filtering rendering and updates the value
-
-    // const dip = document.createElement('p')
-    // dip.textContent = `the total text is ${work.length}`
-    // document.querySelector('#hi').appendChild(dip)
     
-       work.forEach((data)=>{
-        return data.text
-        })
         
     
     
