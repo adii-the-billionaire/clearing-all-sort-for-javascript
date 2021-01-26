84f1ca3e357c8b814e2e0f1060f80005e0df200b
unknown <adiishukla196@gmail.com>
2021-01-26 20:53:39 +0530

    local for notes

--- localStorage/notes/notes-app.js
+++ localStorage/notes/notes-app.js
@@ -1,16 +1,12 @@
-const notes = [{
-    title: 'my next trip',
-    body: 'I would like to go to Spain'
-}, {
-    title: 'Habbits to work on',
-    body: 'Exercise. Eating a bit better.'
-}, {
-    title: 'Office modification',
-    body: 'Get a new seat'
-}]
+let notes = []
 
 const filters = {
     searchText: ''
+}
+
+const notesJSON = localStorage.getItem('notes')
+if(notesJSON!==null){
+    notes =JSON.parse(notesJSON)
 }
 
 const renderNotes = function (notes, filters) {
@@ -22,7 +18,12 @@ const renderNotes = function (notes, filters) {
     
     filteredNotes.forEach(function (note) {
         const noteEl = document.createElement('p')
-        noteEl.textContent = note.title
+        if(note.title.length>0){
+            noteEl.textContent = note.title
+        }else{
+            noteEl.textContent = 'unnamed notes'
+        }
+
         document.querySelector('#notes').appendChild(noteEl)
     })
 }
@@ -30,7 +31,13 @@ const renderNotes = function (notes, filters) {
 renderNotes(notes, filters)
 
 document.querySelector('#create-note').addEventListener('click', function (e) {
+    notes.push({
+        title:'',
+        body:''
+    })
+    localStorage.setItem('notes',JSON.stringify(notes))
     e.target.textContent = 'The button was clicked'
+    renderNotes(notes,filters)
 })
 
 document.querySelector('#search-text').addEventListener('input', function (e) {
