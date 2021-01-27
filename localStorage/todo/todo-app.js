f17000a11cef7189ac9d22e0937b191bff32bc32
unknown <adiishukla196@gmail.com>
2021-01-27 22:36:28 +0530

    set up saving data for checkbox

--- localStorage/todo/todo-app.js
+++ localStorage/todo/todo-app.js
@@ -15,7 +15,7 @@ document.querySelector('#search-text').addEventListener('input', function (e) {
 document.querySelector('#new-todo').addEventListener('submit', function (e) {
     e.preventDefault()
     todos.push({
-        id:'',
+        id:uuidv4(),
         text: e.target.elements.text.value,
         completed: false
     })
--- localStorage/todo/todo-functions.js
+++ localStorage/todo/todo-functions.js
@@ -1,4 +1,5 @@
 // Fetch existing todos from localStorage
+console.log(uuidv4())
 const getSavedTodos = function () {
     const todosJSON = localStorage.getItem('todos')
 
@@ -44,6 +45,15 @@ const renderTodos = function (todos, filters) {
     })
 }
 
+const toggleTodo = (id)=>{
+    const fis = todos.find((todo)=>{
+        return todo.id ===id
+    })
+    if(fis!==undefined){
+        fis.completed = !fis.completed
+    }
+}
+
 // Get the DOM elements for an individual note
 const generateTodoDOM = function (todo) {
     const todoEl = document.createElement('div')
@@ -53,8 +63,14 @@ const generateTodoDOM = function (todo) {
 
     // Setup todo checkbox
     checkbox.setAttribute('type', 'checkbox')
+    checkbox.checked = todo.completed
     todoEl.appendChild(checkbox)
-
+    checkbox.addEventListener('change',()=>{
+        //todo.completed = e.target.checked
+        toggleTodo(todo.id)
+        saveTodos(todos)
+        renderTodos(todos,filters)
+    })
     // Setup the todo text
     todoText.textContent = todo.text
     todoEl.appendChild(todoText)
