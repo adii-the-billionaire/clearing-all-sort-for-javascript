035b0190c82f16582955488d9c074e6003695d04
unknown <adiishukla196@gmail.com>
2021-02-02 17:51:52 +0530

    i think every things is connected

--- localStorage/notes/note-edit.js
+++ localStorage/notes/note-edit.js
@@ -1,6 +1,6 @@
 const titleElement = document.querySelector('#note-title')
 const bodyElement =document.querySelector('#note-body')
-const refreshPage = documenet.querySelector('#save-note')
+const refreshPage = document.querySelector('#save-note')
 const removeElement = document.querySelector('#remove-note')
 const noteId = location.hash.substring(1)
 const notes = getSavedNotes()
@@ -28,6 +28,10 @@ removeElement.addEventListener('click',(e)=>{
     removeNote(note.id)
     saveNotes(notes)
     location.assign('http://127.0.0.1:5500/localStorage/notes/')
+})
+
+refreshPage.addEventListener('click',(e)=>{
+   window.location.reload()
 })
 
 
