f9228ac5f714712d9ea28d83732a5209df94a7b3
unknown <adiishukla196@gmail.com>
2021-02-04 04:23:18 +0530

    everything is clear

--- localStorage/oop/hangman.js
+++ localStorage/oop/hangman.js
@@ -18,18 +18,27 @@ Hangman.prototype.getPuzzle = function(){
 
 Hangman.prototype.makeGuess = function(guess){
     guess = guess.toLowerCase()
+    
     const isUnique = !this.guessedLetters.includes(guess)
     const isBadGuess = !this.word.includes(guess)
     if(isUnique){
         this.guessedLetters.push(guess)
     }
-    if(isUnique && isBadGuess){
-        this.remainingGuesses--
+    if(isUnique && isBadGuess ){
+        this.remainingGuesses>0?this.remainingGuesses-- :this.remainingGuesses="ur value is just over"
     }
 }
 
-const game1 = new Hangman('miya',2)
+const game1 = new Hangman('cccc',2)
 console.log(game1.getPuzzle())
-game1.makeGuess('s')
-game1.makeGuess('p')
-console.log(game1.remainingGuesses)
+// game1.makeGuess('s')
+// game1.makeGuess('p')
+//console.log(game1.remainingGuesses)
+//understanding of key pressing in windows so let's begin now
+window.addEventListener('keypress',(e)=>{
+    // console.log(e)
+    const guess = String.fromCharCode(e.charCode)
+    game1.makeGuess(guess)
+    console.log(game1.getPuzzle())
+    console.log(game1.remainingGuesses)
+})
