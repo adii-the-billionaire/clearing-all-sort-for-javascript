d6db307b469756886d9bb1773911e349f6c144cf
unknown <adiishukla196@gmail.com>
2021-02-04 02:33:43 +0530

    guessing word

--- localStorage/oop/hangman.js
+++ localStorage/oop/hangman.js
@@ -1,14 +1,14 @@
 const Hangman = function(word,remainingGuesses){
     this.word = word.toLowerCase().split('')
     this.remainingGuesses = remainingGuesses
-    this.guessedLetters = ['c']
+    this.guessedLetters = [ ]
 }
 
 Hangman.prototype.getPuzzle = function(){
     let puzzle = ''
     this.word.forEach((letter)=>{
         if(this.guessedLetters.includes(letter)){
-            puzzle+=letter
+             puzzle+=letter
         }else{
             puzzle+='*'
         }
@@ -16,6 +16,20 @@ Hangman.prototype.getPuzzle = function(){
     return puzzle
 }
 
+Hangman.prototype.makeGuess = function(guess){
+    guess = guess.toLowerCase()
+    const isUnique = !this.guessedLetters.includes(guess)
+    const isBadGuess = !this.word.includes(guess)
+    if(isUnique){
+        this.guessedLetters.push(guess)
+    }
+    if(isUnique && isBadGuess){
+        this.remainingGuesses--
+    }
+}
+
 const game1 = new Hangman('miya',2)
 console.log(game1.getPuzzle())
-
+game1.makeGuess('s')
+game1.makeGuess('p')
+console.log(game1.remainingGuesses)
