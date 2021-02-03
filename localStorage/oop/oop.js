8aa1fde97867cd8cc7718b8b532bce8a4f13810b
unknown <adiishukla196@gmail.com>
2021-02-03 20:10:47 +0530

    constructor function

--- localStorage/oop/oop.js
+++ localStorage/oop/oop.js
@@ -2,7 +2,7 @@ const Person = function(fistName,lastName,age){
     this.fistName  = fistName,
     this.lastName = lastName,
     this.age = age
-}
+} 
 const person1 = new Person('Andrew','Mead',27)
 console.log(person1)
 person1.lastName = "tibia"
