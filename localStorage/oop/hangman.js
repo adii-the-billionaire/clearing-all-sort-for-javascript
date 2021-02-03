6dc916b7d375f256acc0671e9054dd5f938c88b1
unknown <adiishukla196@gmail.com>
2021-02-03 20:09:31 +0530

    prototype

---
+++ localStorage/oop/hangman.js
@@ -0,0 +1,6 @@
+const hangman = function(word,remainingWord){
+    this.word = word,
+    this.remainingWord = remainingWord
+}
+const hangman1 = new hangman('cat',2)
+console.log(hangman1)
---
+++ localStorage/oop/oop.js
@@ -0,0 +1,12 @@
+const Person = function(fistName,lastName,age){
+    this.fistName  = fistName,
+    this.lastName = lastName,
+    this.age = age
+}
+const person1 = new Person('Andrew','Mead',27)
+console.log(person1)
+person1.lastName = "tibia"
+console.log(person1)
+const person2 = new Person('adii','shukla',20)
+console.log(person2)
+//here we are making constructor function
