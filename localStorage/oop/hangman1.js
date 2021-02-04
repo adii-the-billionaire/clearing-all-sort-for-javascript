f36ccf1bda6ca1a369a5dc8d5dc3fa3579a25ca4
unknown <adiishukla196@gmail.com>
2021-02-05 03:24:36 +0530

    destructuring of my file

---
+++ localStorage/oop/hangman1.js
@@ -0,0 +1,74 @@
+class Hangman {
+    constructor(word,remainingGuesses){
+        this.word = word.toLowerCase().split('')
+        this.remainingGuesses = remainingGuesses
+        this.guessedLetters = [ ]
+        this.status = 'playing'
+    }
+
+    getStatusMessage(){
+        if(this.status ==='playing'){
+            return `Guesses left: ${this.remainingGuesses}`
+        }else if(this.status==='failed'){
+            return `Nice try ! The word was ""${this.word.join('')}"" `
+        }else{
+            return `Great work ! You  guessed the word`
+        }
+    }
+
+    calculateStatus(){
+        const finished = this.word.every((letter)=>{
+            return this.guessedLetters.includes(letter)|| letter===' '
+        })
+        // const lettersUnguessed = this.word.filter((letter)=>{
+        //     return !this.guessedLetters.includes(letter)
+        // })
+        // const finished = lettersUnguessed.lenght==='0'
+        // let finished = true
+        // this.word.forEach((letter)=>{
+        //     if(this.guessedLetters.includes(letter)){
+        //         finished = true
+        //     }else{
+        //         finished = false
+        //     }
+        // })
+        if(this.remainingGuesses===0){
+            this.status = 'failed'
+        }else if(finished){
+            this.status = 'finished'
+        }else{
+            this.status ='playing'
+        }
+    }
+
+    getPuzzle(){
+        let puzzle = ''
+        this.word.forEach((letter)=>{
+            if(this.guessedLetters.includes(letter)||letter===' '){
+                 puzzle+=letter
+            }else{
+                puzzle+='*'
+            }
+        })
+        return puzzle
+    }
+
+    makeGuess(){
+        guess = guess.toLowerCase()
+        
+        const isUnique = !this.guessedLetters.includes(guess)
+        const isBadGuess = !this.word.includes(guess)
+        if(this.status!=='playing'){
+            return
+        }
+        if(isUnique){
+            this.guessedLetters.push(guess)
+        }
+        if(isUnique && isBadGuess ){
+            this.remainingGuesses>0?this.remainingGuesses-- :this.remainingGuesses=" over"
+        }
+        this.calculateStatus()
+    }
+
+}
+
