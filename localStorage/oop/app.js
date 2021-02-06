a50771492809c9cb5b94b8100b5d3321d02f2776
unknown <adiishukla196@gmail.com>
2021-02-07 04:47:22 +0530

    final caLL

--- localStorage/oop/app.js
+++ localStorage/oop/app.js
@@ -2,11 +2,9 @@ const mia = document.querySelector('#sia')
 const nip = document.querySelector('#nip')
 const sia = document.querySelector('#mia')
 
-const game1 = new Hangman('cia',2)
+let game1 
 
-sia.textContent = game1.getStatusMessage
-nip.textContent = ` your remaining guess is ${game1.remainingGuesses}`
-mia.textContent = game1.getPuzzle()
+
 
 window.addEventListener('keypress',(e)=>{
     // console.log(e)
@@ -14,10 +12,33 @@ window.addEventListener('keypress',(e)=>{
     game1.makeGuess = guess
     console.log(game1.getPuzzle())
     console.log(game1.remainingGuesses)
-    mia.textContent = game1.getPuzzle()
-    nip.textContent = ` your remaining guess is ${game1.remainingGuesses}`
-    sia.textContent = game1.getStatusMessage
+    render()
 })
+
+getPuzzle(2).then((data)=>{
+    console.log(data.puzzle)
+}).catch((e)=>{
+    console.log(e)
+})
+
+const generate = async()=>{
+    const word = await getPuzzle(2)
+    console.log(word)
+    game1= new Hangman(word.puzzle,5)
+    render()
+}
+ 
+generate()
+
+const render = ()=>{
+    sia.textContent = game1.getStatusMessage
+    nip.textContent = ` your remaining guess is ${game1.remainingGuesses}`
+    mia.textContent = game1.getPuzzle()
+}
+
+
+
+document.querySelector('#jia').addEventListener('click',generate)
 
 // getPuzzle.then((data)=>{
 //     console.log(data)
@@ -43,19 +64,19 @@ window.addEventListener('keypress',(e)=>{
 // })
 
 //here is we are using arguement as function that become callback so let's start the 
-const getLocation = ()=>{
-    return fetch('http://ipinfo.io/json?token=ce6d62364d5c83').then((response)=>{
-        if(response.status===200){
-            return response.json()
-        }else{
-            throw new Error('error is error')
-        }
-    })
-}
-getLocation().then((data)=>{
-    console.log(data)
-    console.log(data.country)
-    getCountry(`${data.country}`)
-}).catch((e)=>{
-    console.log(e)
-})
+// const getLocation = ()=>{
+//     return fetch('http://ipinfo.io/json?token=ce6d62364d5c83').then((response)=>{
+//         if(response.status===200){
+//             return response.json()
+//         }else{
+//             throw new Error('error is error')
+//         }
+//     })
+// }
+// getLocation().then((data)=>{
+//     console.log(data)
+//     console.log(data.country)
+//     getCountry(`${data.country}`)
+// }).catch((e)=>{
+//     console.log(e)
+// })
--- localStorage/oop/request3.js
+++ localStorage/oop/request3.js
@@ -1,31 +1,38 @@
-const getPuzzle = (word)=>{
-    return fetch(`http://puzzle.mead.io/puzzle?wordCount=${word}`).then((response)=>{
-    if(response.status===200){
-        return response.json()
-    }
-}).then((data)=>{
-    return console.log(data.puzzle)
-}).catch((e)=>{
-    console.log(e)
-})
+const getPuzzle = async (word)=>{
+    const data = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${word}`)
+    return data.json()
 }
-getPuzzle(3)
 
 
- const getCountry = (code)=>{
-     return fetch('https://restcountries.eu/rest/v2/all').then((response)=>{
-         if(response.status===200){
-             return response.json()
-         }
-     }).then((data)=>{
-        const myData = data.filter((data)=>{
-            return data.alpha2Code === 'IN'
-        })
-        return console.log(myData[0].name)
-     }).catch((e)=>{
-         console.log('something proble')
-     })
- }
+// const getPuzzle = (word)=>{
+
+//     return fetch(`http://puzzle.mead.io/puzzle?wordCount=${word}`).then((response)=>{
+//     if(response.status===200){
+//         return response.json()
+//     }
+// }).then((data)=>{
+//     return console.log(data.puzzle)
+// }).catch((e)=>{
+//     console.log(e)
+// })
+// }
+// getPuzzle(3)
+
+
+//  const getCountry = (code)=>{
+//      return fetch('https://restcountries.eu/rest/v2/all').then((response)=>{
+//          if(response.status===200){
+//              return response.json()
+//          }
+//      }).then((data)=>{
+//         const myData = data.filter((data)=>{
+//             return data.alpha2Code === 'IN'
+//         })
+//         return console.log(myData[0].name)
+//      }).catch((e)=>{
+//          console.log('something proble')
+//      })
+//  }
  
  
  
