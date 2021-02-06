11f1177d4eb118ad98a03689d7f39330a3ca1dfe
unknown <adiishukla196@gmail.com>
2021-02-07 00:15:13 +0530

    submission

--- localStorage/httpAsyncHttps/fetch.js
+++ localStorage/httpAsyncHttps/fetch.js
@@ -1,9 +1,3 @@
 //here we learning fetch api 
 //that build promise chaining
-fetch('http://puzzle.mead.io/puzzle').then((response)=>{
-    return response.json() 
-}).then((data)=>{
-    console.log(data)
-}).catch((e)=>{
-    console.log(e)
-})
+
