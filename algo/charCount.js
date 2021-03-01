b734352ce21a37eacdd23682ae9ffaa66f4fcbf6
unknown <adiishukla196@gmail.com>
2021-03-02 00:46:23 +0530

    pseudocode of charcount

--- algo/charCount.js
+++ algo/charCount.js
@@ -1,9 +1,13 @@
 function charCount(str){
     let obj = {}
     for(let i =0;i<str.length;i++){
-        obj[str[i]] = 0
+        if(obj[str[i]]>0){
+            obj[str[i]]++
+        }else{
+            obj[str[i]] = 1
+        }
     }
     return obj
 }
-const mia = charCount('ciaodios')
+const mia = charCount('ccciaodios')
 console.log(mia)
