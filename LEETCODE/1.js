37de5d7c9c024d65491d3316ab5440a078e59a2b
unknown <adiishukla196@gmail.com>
2021-03-09 07:36:54 +0530

    using sort

--- LEETCODE/1.js
+++ LEETCODE/1.js
@@ -1,4 +1,22 @@
-function main(arr) {
+function main(arr, target) {
+    let newArr = arr.sort((a, b) => {
+        return a - b
+    })
+    console.log(newArr)
+    let start = 0
+    let end = newArr.length - 1
+    while (start < end) {
+        let sum = newArr[start] + newArr[end]
+        if (sum === target) {
+            return [newArr[start], newArr[end]]
+        } else
+        if (sum > target) {
+            end--
+        } else {
+            start++
+            console.log(start)
+        }
+    }
 
 }
-console.log(main(arr))
+console.log(main([2, 7, 11, 15], 9))
